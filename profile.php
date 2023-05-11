ae41783f643be2d38744453c95f5ccc6fd88640d
Allain Daniel Espina <allaindaniel.espina.cics@ust.edu.ph>
2023-05-11 17:51:32 +0700

    Made some minor changes to PWUpdate Files

--- profile.php
+++ profile.php
@@ -7,22 +7,6 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: indexLogin.html');
 	exit;
 }
-// $DATABASE_HOST = 'localhost';
-// $DATABASE_USER = 'root';
-// $DATABASE_PASS = '';
-// $DATABASE_NAME = 'dost';
-// $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-// if (mysqli_connect_errno()) {
-// 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-// }
-// // We don't have the password or email info stored in sessions, so instead, we can get the results from the database.
-// $stmt = $con->prepare('SELECT password, email, name FROM accounts WHERE id = ?');
-// // In this case we can use the account ID to get the account info.
-// $stmt->bind_param('i', $_SESSION['id']);
-// $stmt->execute();
-// $stmt->bind_result($password, $email);
-// $stmt->fetch();
-// $stmt->close();
 ?>
 <!DOCTYPE html>
 <html>
@@ -80,13 +64,9 @@ if (!isset($_SESSION['loggedin'])) {
 					<td>Email:</td>
 					<td><?php echo $email ?></td>
 				</tr>
-				<!-- <tr>
-					<td>Password:</td>
-					<td><?php $password ?></td>
-				</tr> -->
 			</table>
 			<br><br>
-			<form method="post" action="VerifyUserPWUpdate.html">
+			<form method="post" action="PWUpdate.html">
 				<input type="submit" id="changePW" value="Change Password" />
 			</form>
 		</div>
