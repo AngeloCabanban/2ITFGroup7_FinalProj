d7214d0c5c0246b1f0cada035b4e197d272a0897
Fritz Cabe <fritzgerald.cabe.cics@ust.edu.ph>
2023-05-16 22:34:02 +0800

    fixed login page and added error page

--- authenticate.php
+++ authenticate.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<script src="bootstrap/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+</body>
+</html>
+
 <?php
 session_start();
 // Change this to your connection info.
@@ -37,13 +52,27 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE email = ?')) 
             $_SESSION['name'] = $_POST['email'];
             $_SESSION['id'] = $id;
             header('Location: home.php');
-        } else {
-            // Incorrect password
-            echo 'Incorrect Password!';
+        } else { 
+            header('Location: ErrorPassword.php');
         }
-    } else {
-        // Incorrect username
-        echo 'Username not found!';
+    } else { 
+                    ?>
+                                <div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSheet">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content rounded-4 shadow">
+                    <div class="modal-header border-bottom-0">
+                        <h1 class="modal-title fs-5">ERROR</h1>
+                    </div>
+                    <div class="modal-body py-0">
+                        <p>Incorrect Username</p>
+                    </div>
+                    <div class="modal-footer flex-column align-items-stretch w-100 gap-2 pb-3 border-top-0">
+                    <a href="indexLogin.html"> <button type="button" class="btn btn-lg btn-primary">Go back</button></a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            <?php 
     }
 
     $stmt->close();
