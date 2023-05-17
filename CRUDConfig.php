4d2de30651d4df5819195df1a1244056b66c295a
Allain Daniel Espina <allaindaniel.espina.cics@ust.edu.ph>
2023-05-17 19:29:39 +0700

    Slight modifications to some files and added password encryption

--- CRUDConfig.php
+++ CRUDConfig.php
@@ -13,3 +13,8 @@ $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if ($mysqli === false) {
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
+
+// INSERT STATEMENT FOR NEW ACCOUNTS:
+// INSERT INTO `accounts` (`id`, `name`, `email`, `password`) VALUES (NULL, 'SITE', 'site@cics.com', MD5('site'));
+
+?>
