9e891d34e3972027b2875a34ad1264a1ab39979e
Allain Daniel Espina <allaindaniel.espina.cics@ust.edu.ph>
2023-05-23 08:20:25 +0700

    Fixed change password error messages

--- PWUpdateAuthenticate.php
+++ PWUpdateAuthenticate.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+</body>
+
+</html>
+
 <?php
 require_once "CRUDConfig.php";
 session_start();
@@ -30,7 +48,12 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE email = ?')) 
         $stmt->fetch();
         // Account exists, now we verify the password.
         // Note: remember to use password_hash in your registration file to store the hashed passwords.
-        if (MD5(password_verify($_POST['password'], $password))) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $result = $con->query("SELECT * FROM accounts WHERE email='" . $email . "' AND password='" . md5($password) . "'");
+
+        if ($result->num_rows) {
             if ($_POST['newPW'] === $_POST['repeatNewPW']) {
                 // Verification success! User has logged-in!
                 // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
@@ -44,53 +67,51 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE email = ?')) 
                 $uquery = mysqli_query($mysqli, $updatequery);
 
                 header('Location: profile.php');
-            } else {
+            } else
 ?>
 
-                <div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSheet">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content rounded-4 shadow">
-                            <div class="modal-header border-bottom-0">
-                                <h1 class="modal-title fs-5">ERROR</h1>
-                            </div>
-                            <div class="modal-body py-0">
-                                <p>New and Repeated Passwords do not match!</p>
-                            </div>
-                            <div class="modal-footer flex-column align-items-stretch w-100 gap-2 pb-3 border-top-0">
-                                <a href="PWUpdate.html"> <button type="button" class="btn btn-lg btn-primary">Go back</button></a>
-                            </div>
+            <div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSheet">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content rounded-4 shadow">
+                        <div class="modal-header border-bottom-0">
+                            <h1 class="modal-title fs-5">ERROR</h1>
+                        </div>
+                        <div class="modal-body py-0">
+                            <p>New and Repeated Passwords do not match!</p>
+                        </div>
+                        <div class="modal-footer flex-column align-items-stretch w-100 gap-2 pb-3 border-top-0">
+                            <a href="PWUpdate.html"> <button type="button" class="btn btn-lg btn-primary">Go back</button></a>
                         </div>
                     </div>
                 </div>
+            </div>
 
-        <?php
-            }
-        } else {
-            // Incorrect password
-            header('Location: ErrorPassword.php');
-        }
-    } else {
-        // Incorrect username
-        ?>
+    <?php
+    }
+} else {
+    // Incorrect password
+    header('Location: ErrorPassword.php');
+}
+} else {
+    // Incorrect username
+    ?>
 
-        <div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSheet">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content rounded-4 shadow">
-                    <div class="modal-header border-bottom-0">
-                        <h1 class="modal-title fs-5">ERROR</h1>
-                    </div>
-                    <div class="modal-body py-0">
-                        <p>Incorrect Username</p>
-                    </div>
-                    <div class="modal-footer flex-column align-items-stretch w-100 gap-2 pb-3 border-top-0">
-                        <a href="indexLogin.html"> <button type="button" class="btn btn-lg btn-primary">Go back</button></a>
-                    </div>
+    <div class="modal modal-sheet position-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSheet">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content rounded-4 shadow">
+                <div class="modal-header border-bottom-0">
+                    <h1 class="modal-title fs-5">ERROR</h1>
+                </div>
+                <div class="modal-body py-0">
+                    <p>Incorrect Username</p>
+                </div>
+                <div class="modal-footer flex-column align-items-stretch w-100 gap-2 pb-3 border-top-0">
+                    <a href="indexLogin.html"> <button type="button" class="btn btn-lg btn-primary">Go back</button></a>
                 </div>
             </div>
         </div>
+    </div>
 
 <?php
-    }
-
     $stmt->close();
 }
